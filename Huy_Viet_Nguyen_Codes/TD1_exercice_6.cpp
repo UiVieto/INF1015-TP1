2e0f60fef41bc8304eb866d7e3e256bf2911b929
Huy Viet Nguyen <90419850+UiVieto@users.noreply.github.com>
2022-01-28 18:28:34 -0500

    Version finale

--- Huy_Viet_Nguyen_Codes/TD1_exercice_6.cpp
+++ Huy_Viet_Nguyen_Codes/TD1_exercice_6.cpp
@@ -1,23 +1,97 @@
-// Entête
+/**
+* Programme qui crée un dictionnaire en tant qu'array de strutures à partir d'un fichier texte et qui affiche le mot le plus du dictionnaire.
+* \file   TD1_exercice_6.cpp
+* \author Huy Viet Nguyen et Reda Rhanmouni (Section 5)
+* \date   30 janvier 2022
+* Créé le 23 janvier 2022
+*/
 
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <fstream>
 #include <vector>
 using namespace std;
 
-string lireLigne();
-
-struct Dictionnaire
+struct Mot 
 {
-    string mot;
-    string natureGenre;
+    string orthographe;
+    string nature;
     string definition;
 };
 
+int trouverNombreMotsDictionnaire();
+vector<Mot> creerDictionnaire();
+Mot trouverNomPlusLong(vector<Mot> dictionnaire);
+
+
 int main()
 {
-    fstream fichierDictionnaire;
-    fichierDictionnaire.open("dictionnaire.txt", ios::in);
+    vector<Mot> dictionnaire = creerDictionnaire();
+    Mot nomPlusLong = trouverNomPlusLong(dictionnaire);
 
+    cout << nomPlusLong.orthographe << " (" << nomPlusLong.nature << ") : " << nomPlusLong.definition;
+
+    return 0;
+}
+
+
+int trouverNombreMotsDictionnaire()
+{
+    std::ifstream fichier("dictionnaire.txt");
+    std::string element;
+    string ligne;
+    int nombreMots = 0;
+
+    while (std::getline(fichier, ligne))
+    {
+        nombreMots++;
+    }
+
+    return nombreMots;
+}
+
+vector<Mot> creerDictionnaire()
+{
+    std::ifstream fichier("dictionnaire.txt");
+    std::string element;
+
+    vector<Mot> dictionnaire;
+
+    string mot;
+    string nature;
+    string definition;
+
+    for (int i = 1; i <= trouverNombreMotsDictionnaire(); i++)
+    {
+        Mot nouveauMot;
+
+        std::getline(fichier, mot, '\t');
+        nouveauMot.orthographe = mot;
+
+        std::getline(fichier, nature, '\t');
+        nouveauMot.nature = nature;
+
+        std::getline(fichier, definition, '\n');
+        nouveauMot.definition = definition;
+
+        dictionnaire.push_back(nouveauMot);
+    }
+
+    return dictionnaire;
+}
+
+Mot trouverNomPlusLong(vector<Mot> dictionnaire)
+{   
+    int nombreMots = trouverNombreMotsDictionnaire();
+    Mot nomPlusLong = dictionnaire[0];
+
+    for (int i = 1; i < nombreMots; i++)
+    {
+        if ((nomPlusLong.orthographe).size() > (dictionnaire[i].orthographe).size())
+            continue;
+        else
+            nomPlusLong = dictionnaire[i];
+    }
+
+    return nomPlusLong;
 }
